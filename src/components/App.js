b6783fc2060f31344f5b46cbade8ba699d588264
+ <+>
2023-04-21 11:28:16 +0300

    Done with Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import MovieCard from "./MovieCard";
 
 function App() {
-  const title = "Mad Max";
-  const posterURL =
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
-  const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
-
+  const movie1 = {
+    title: "Mad Max",
+    posterURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM",
+    genresArr: ["Action", "Adventure", "Science Fiction", "Thriller"],
+  };
+  const movie2 = {
+    title: "The Shawshank Redemption",
+    posterURL: "https://c8.alamy.com/comp/BKH04B/the-shawshank-redemption-1994-poster-shwk-003vs-BKH04B.jpg",
+    genresArr: ["Drama"]
+  };
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={movie1.title} genres={movie1.genresArr} />
+      <MovieCard title={movie2.title} posterSrc={movie2.posterURL} genres={movie2.genresArr} />
     </div>
   );
 }
